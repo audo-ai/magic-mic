d96385bef74f4edb141b31c378732d281eb872f3
Gabe Cohen <gcc3.14@gmail.com>
2021-02-12 12:22:18 -0600

    I'm very stupid

--- native/denoiser/denoiser.cc
+++ native/denoiser/denoiser.cc
@@ -8,11 +8,9 @@ Denoiser::Denoiser(std::string ts_path) {
     //.layout(torch::kStrided)
     //.device(torch::kCPU, 0)
                      .requires_grad(false);
-  torch::jit::script::Module module;
   try {
     // Deserialize the ScriptModule from a file using torch::jit::load().
     module = torch::jit::load(ts_path);
-    module.dump(true, false, false);
   }
   catch (const c10::Error& e) {
     throw e.msg();
@@ -28,9 +26,6 @@ Denoiser::Denoiser(std::string ts_path) {
        torch::zeros(c10::IntArrayRef({1, 48, 4}), options),
        torch::zeros(c10::IntArrayRef({1, 1, 4}), options)});
 }
-// TODO since pulseaudio can't touch pytorch we need to construct tensors here,
-// which means we either have to copy, or take a deleter. I think taking a
-// deleter would be better
 void Denoiser::feed(float *arr, std::size_t size) {
   in.insert<float*>(in.end(), arr, arr+size);
   std.feed(arr, size);
@@ -45,7 +40,6 @@ std::size_t Denoiser::spew(float *out, std::size_t maxsize) {
   int chunk;
   for (chunk = 1; chunk * valid_length <= maxsize && chunk*valid_length <= in.size(); chunk++) {
     torch::Tensor audio = torch::from_blob((float *)in.data() + (chunk-1)*valid_length, valid_length, options);
-    std::cout<<audio.numel();
     audio /= std.std();
     audio = audio.view(c10::IntArrayRef({1,1,valid_length}));
     torch::jit::IValue model_out = module.forward(std::vector<torch::IValue>({torch::jit::IValue(audio), torch::jit::IValue(conv_hidden), torch::jit::IValue(lstm_hidden)}));
