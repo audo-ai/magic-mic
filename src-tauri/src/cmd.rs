f478de9ce03396e93c193bff754bcd2a106734e1
Gabe Cohen <gcc3.14@gmail.com>
2021-02-27 23:06:45 -0600

    Add tauri server IPC

--- src-tauri/src/cmd.rs
+++ src-tauri/src/cmd.rs
@@ -6,5 +6,30 @@ pub enum Cmd {
   // your custom commands
   // multiple arguments are allowed
   // note that rename_all = "camelCase": you need to use "myCustomCommand" on JS
-  MyCustomCommand { argument: String },
+  GetStatus {
+    callback: String,
+    error: String,
+  },
+  SetShouldRemoveNoise {
+    value: bool,
+    callback: String,
+    error: String,
+  },
+  SetMicrophone {
+    value: i32,
+    callback: String,
+    error: String
+  },
+  GetMicrophones {
+    callback: String,
+    error: String,
+  },
+  Exit,
 }
+// #[derive(Deserialize, Debug)]
+// #[serde(tag = "resp", rename_all = "camelCase")]
+// pub enum Resp {
+//   Ack,
+//   Status(bool),
+//   Error(String),
+// 
