0e24ecbed473a81f7db0490577d7202ffa3e68bf
Gabe Cohen <gcc3.14@gmail.com>
2021-03-06 14:37:55 -0600

    Improving build system Automatically fetch (most) dependencies. Moving towards a docker build

--- scripts/beforeBuildCommand.sh
+++ scripts/beforeBuildCommand.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # For now tauri seems to run commands very stupidly (ie. doesn't pass to shell,
 # just splits on spaces and execs). So we do this
-cd ./src-web && yarn build
+cd ./src-web && npx --no-install yarn build
